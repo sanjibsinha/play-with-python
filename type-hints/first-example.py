7906ef1e35632f7590fc863d684c499eab234085
Sanjib Sinha <sanjib12sinha@gmail.com>
2025-01-09 06:25:17 +0530

    first commit

--- type-hints/first-example.py
+++ type-hints/first-example.py
@@ -1,5 +1,14 @@
 # type hints in python
 # this is a comment
 
-def add(a: int, b: int) -> int:
-    return a + b
+def returning_string(name : str) -> str:
+    """
+    This function takes a string as an argument and returns a string
+    
+    """
+    string_to_return = f"Hello {name}"
+    return string_to_return
+
+# get the return value, or string
+returned_string = returning_string("John")
+print(returned_string)
